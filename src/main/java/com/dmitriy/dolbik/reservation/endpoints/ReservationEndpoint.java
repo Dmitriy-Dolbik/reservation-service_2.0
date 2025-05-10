fcd345e7add48dbb530f8c8a0c0cce1c81e791df
Dmitriy.Dolbik <dda1508@gmail.com>
2025-05-10 11:06:44 +0300

    Add endpoint to get all reservations

---
+++ src/main/java/com/dmitriy/dolbik/reservation/endpoints/ReservationEndpoint.java
@@ -0,0 +1,33 @@
+package com.dmitriy.dolbik.reservation.endpoints;
+
+import com.dmitriy.dolbik.reservation.models.Reservation;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+@Path("reservation")//в этом классе будем обрабатывать запросы с путем /reservation
+public class ReservationEndpoint {
+
+    @GET//обрабатываем HTTP GET запросы
+    @Path("all")//при обращении по пути /reservation/all попадем в этот метод
+    public List<Reservation> getAllReservations() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return List.of(
+                new Reservation(
+                        1L,
+                        "userId_1",
+                        1L,
+                        LocalDate.parse("10.05.2025", formatter),
+                        LocalDate.parse("15.05.2025", formatter)),
+                new Reservation(
+                        2L,
+                        "userId_2",
+                        2L,
+                        LocalDate.parse("15.05.2025", formatter),
+                        LocalDate.parse("20.05.2025", formatter))
+        );
+    }
+}
